03a794fad2dbf14ec1e9c6276e90fd396c391a13
lola645268@gmail.com < >
2021-10-21 09:19:17 +0300

    IsLeapYear

---
+++ Program1.pas
@@ -0,0 +1,12 @@
+﻿function IsLeapYear(y: integer): boolean;
+begin
+  if (((y mod 4) = 0) and ((y mod 100) <> 0)) or ((y mod 400) = 0) then
+    result := True
+  else
+    result := False;
+end;
+
+
+begin
+
+end.
